ad7f956cea08788dedcf08e9c6854babf4f76cbf
Hareem2134 <007hareem@gmail.com>
2024-07-02 09:09:58 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "What do you want to add in your Todos?",
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more?",
+            default: false, // this should be a boolean value
+        },
+    ]);
+    todos.push(addTask.todo);
+    condition = addTask.addMore;
+    console.log(todos);
+}
